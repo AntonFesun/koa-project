f1c45020b34957835fb6bfa7554a950804ce96bf
Andy <karandriy@gmail.com>
2019-08-02 12:12:13 +0300

    path

--- config/production .js
+++ config/production .js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT,
-    databaseUrl: 'mongodb://AntonFesun:rpdstklmn1A@ds135776.mlab.com:35776/heroku_zt8092sg'
+    databaseUrl: process.env.MONGODB_URI
 };
