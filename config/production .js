f4bb621874aa1881de3c39ba645eb37ec0cf8d8c
Andy <karandriy@gmail.com>
2019-08-02 11:41:07 +0300

    path to mongo

--- config/production .js
+++ config/production .js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.PORT,
-    MONGODB_URI: 'mongodb://heroku_zt8092sg:pjtrkkhpqvaatjb51nrc83u42a@ds135776.mlab.com:35776/heroku_zt8092sg'
+    databaseUrl: 'mongodb://<dbuser>:<dbpassword>@ds135776.mlab.com:35776/heroku_zt8092sg'
 };
