c41dc9649886a159a17ead5705ddbca38f79870f
Andy <karandriy@gmail.com>
2019-08-02 14:59:50 +0300

    local mongodb link

--- config/default.js
+++ config/default.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: 3000,
-  databaseUrl: "mongodb+srv://AntonFesun:rpdstklmn1A@cluster0-3ulyp.mongodb.net/test?retryWrites=true&w=majority"
+  databaseUrl: 'mongodb://localhost:27017/test'
 };
